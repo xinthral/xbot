fc99c530a905172a48a2d36ed7dd84b31e5b1b29
Jesse Hassell <xinthral@yahoo.com>
2020-02-02 21:03:05 -0500

    Re-orging large text content

---
+++ db/phrases_db.py
@@ -0,0 +1,17 @@
+phraseDict = {
+    'positivity' : [
+        ["I\'m on that positivity flow, I\'ll blind the darkness with my glow, we out here letting everyone know, it\'s going down SO LETS GO!!!!"],
+    ],
+    'inspirational' : [
+        ["You can choose to love, or you can choose to hate. But you only have enough energy for one, so choose wisely."],
+    ],
+    'squotes': [
+        ["Who\'s that cummin up in here with the BDE!?!?"],
+        ["Rando: I waited 18 minutes for this? Panda: I\'ve waited my whole life to have sex and it\'s only like 30 seconds."],
+        ["You done fk\'d up A-A-ron!"],
+    ],
+    'rquotes': [
+        ["I\'d rather die exhausted, than to live with regrets."],
+        ["You can\'t make something idiot proof cause God designs the idiots, and he\'s a better bot designer than you."],
+    ]
+}
