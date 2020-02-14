0cad0c942b6e9fabf2e876d336c8222b11d1cbfa
Jesse <jjhassel@mtu.edu>
2020-02-14 13:41:31 -0500

    Bolstered the Dictionaries

--- db/phrases_db.py
+++ db/phrases_db.py
@@ -1,17 +1,34 @@
 phraseDict = {
     'positivity' : [
         ["I\'m on that positivity flow, I\'ll blind the darkness with my glow, we out here letting everyone know, it\'s going down SO LETS GO!!!!"],
+        ["The most important thing you will ever wear is your attitude."],
+        ["Dr. Barbara Fredrickson (a medical researcher) discovered that experiencing positive emotions broadens people\'s minds and builds their resourcefulness in ways that help them become more resilient to adversity."],
+        ["Once you replace your negative thoughts with positive ones, you\'ll start having positive results. -- Willie Nelson"],
+        ["I always try to look on the optimistic side, but I am realistic enough to know that life is a complex matter. -- Walt Disney"],
+        ["You can\'t make positive choices for the rest of your life without an environtment that makes those choices easy, natural, and enjoyable. -- Deepak Chopra"],
+        ["Perpetual optimism is a force multiplier. -- Colin Powell"],
+        ["Failures are simply steps you must climb to reach to the door of success. -- Xinthral"],
+        ["Optimism is a happiness magnet. If you stay positive, good things and good people will be drawn to you. -- Marry Lou Retton"]
     ],
     'inspirational' : [
-        ["You can choose to love, or you can choose to hate. But you only have enough energy for one, so choose wisely."],
+        ["You can choose to love, or you can choose to hate. But you only have enough energy for one, so choose wisely. -- Dad"],
+        ["You can, you should, and if you\'re brave enough, you will. -- Stephen King"],
+        ["Happiness is a (measurement of the) quality of the soul, not a function of one\'s material circumstances. -- Aristotle"],
+        ["A powerful attitude awakens inner strength, energy, motivation, and initiative. -- Remez Sasson"],
+        ["Yesterday is not ours to recover, but tomorrow is ours to win or lose. -- Lyndon B. Johnson"],
+        ["I\'d rather die exhausted, than to live with regrets. -- T3chnopanda"],
+        ["You\'re off to great places, today is your day. Your mountain is waiting, so get on your way. -- Dr. Seuss"],
+        ["No one is perfect, that\'s why pencils have erasers. -- Wolfgang Riebe"],
+        ["It always seems impossible until it is done. -- Nelson Mandela"],
+        ["The sun (himself) is weak when he first rises, and gathers strength and corage as the day gets on. -- Charles Dickens"]
     ],
-    'squotes': [
-        ["Who\'s that cummin up in here with the BDE!?!?"],
-        ["Rando: I waited 18 minutes for this? Panda: I\'ve waited my whole life to have sex and it\'s only like 30 seconds."],
+    'stream': [
+        ["Who\'s that cummin up in here with the BDE!?!? -- T3chnopanda"],
+        ["Rando: I waited 18 minutes for this to end so quickly? T3chnopanda: I waited my whole life to have sex and it only took 30 seconds."],
         ["You done fk\'d up A-A-ron!"],
     ],
-    'rquotes': [
-        ["I\'d rather die exhausted, than to live with regrets."],
-        ["You can\'t make something idiot proof cause God designs the idiots, and he\'s a better bot designer than you."],
+    'funny': [
+        ["You can\'t make something idiot-proof cause God designs the idiots, and he\'s a better bot designer than you. -- Xinthral"],
+        [""],
     ]
 }
