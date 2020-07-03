7fa0193616598390ceebfca264056abf10a5c879
Jesse <jjhassel@mtu.edu>
2020-07-03 13:07:50 -0400

    Implemented Launch method for initialization options.

--- launch.py
+++ launch.py
@@ -1,34 +1,6 @@
-import irc.bot
-import json
-import requests
 import sys
-
 from utils import cnf
-
-class xBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, channel, username, client_id, token):
-        self.username = username
-        self.client_id = client_id
-        self.token = token
-        self.channel = channel
-
-        # Get the channel id, we will need this for v5 API calls
-        url = 'https://api.twitch.tv/helix/users?login=' + channel
-        self.headers = {'Client-ID': client_id,
-            'Authorization': 'Bearer ' + token,
-            'Accept': 'application/vnd.twitchtv.helix+json',
-        }
-        r = requests.get(url, headers=self.headers).json()
-        print(r)
-
-        # Create IRC bot connection
-        server = 'irc.chat.twitch.tv'
-        port = 6667
-        print('Connecting to ' + server + ' on port ' + str(port) + '...')
-        irc.bot.SingleServerIRCBot.__init__(self,
-            [(server, port, 'oauth:' + token)],
-            username, username)
-    pass
+from xBot import NerdKommander
 
 def main():
     if len(sys.argv) > 1:
@@ -38,12 +10,12 @@ def main():
     else:
         channel = cnf('AUTH', 'CHANNEL_ID')
 
-    username  = cnf('AUTH', 'IDENT')
+    username  = cnf('AUTH', 'NICK')
     client_id = cnf('AUTH', 'CLIENT_ID')
     token     = cnf('AUTH', 'ACCESS_TOKEN')
 
     try:
-        bot = xBot(channel, username, client_id, token)
+        bot = NerdKommander(channel, username, client_id, token)
         bot.start()
     except KeyboardInterrupt:
         print("Shutting down...")
